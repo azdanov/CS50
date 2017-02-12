a236747cee13f512f5f1708b41306533b67ebddb
Anton Zdanov <akumael@gmail.com>
2017-02-12 17:17:36 +0200

    Add key check and convert key to int

--- PSET2/caesar.c
+++ PSET2/caesar.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    // check entered key
+    if (argc != 2 || atoi(argv[1]) < 0) {
+        printf("Usage: ./caesar k\nWhere k is a non negative integer.\n");
+        return 1;
+    }
+
+    int key = atoi(argv[1]);
+
+
 
     return 0;
 }
