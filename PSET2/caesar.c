3db9b429bd7c7e6d2b23b2d3a05cbea7db56cf38
Anton Zdanov <akumael@gmail.com>
2017-02-13 04:59:12 +0200

    Finish cypher script Include ctype.h Finish for loop

--- PSET2/caesar.c
+++ PSET2/caesar.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
+
 #define ALPHA_NUM 26
 
 int main(int argc, char *argv[]) {
@@ -18,15 +20,24 @@ int main(int argc, char *argv[]) {
     string plaintext = get_string();
     printf("\n");
 
-    string ciphertext = "";
 
     // encode plaintext
     size_t text_len = strlen(plaintext);
-    for (int i = 0; i < text_len; ++i) {
-        
+    char ciphertext[text_len + 1];
+
+    for (int i = 0; i < text_len; i++) {
+        if (isalpha(plaintext[i])) {
+            if (isupper(plaintext[i])) {
+                ciphertext[i] = (char) (((plaintext[i] + key - 'A') % ALPHA_NUM) + 'A');
+            } else {
+                ciphertext[i] = (char) (((plaintext[i] + key - 'a') % ALPHA_NUM) + 'a');
+            }
+        } else {
+            ciphertext[i] = plaintext[i];
+        }
     }
 
-
+    printf("ciphertext: %s\n", ciphertext);
 
     return 0;
 }
