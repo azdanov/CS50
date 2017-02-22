85a1102fce76e845fe2ea5456ffcdf172a441471
Anton Zdanov <akumael@gmail.com>
2017-02-22 15:38:13 +0200

    Done b-search function

--- PSET3/find_less/helpers.c
+++ PSET3/find_less/helpers.c
@@ -3,7 +3,7 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
+
 #include <cs50.h>
 
 #include "helpers.h"
@@ -11,17 +11,29 @@
 /**
  * Returns true if value is in array of n values, else false.
  */
-bool search(int value, int values[], int n)
-{
-    // TODO: implement a searching algorithm
+bool search(int value, int values[], int n) {
+
+    int mid,
+        lower = 0,
+        upper = n - 1;
+
+    do {
+        mid = (lower + upper) / 2;
+        if (value < values[mid])
+            upper = mid - 1;
+        else if (value > values[mid])
+            lower = mid + 1;
+        else
+            return true;
+    } while (mid > 0 && mid < n - 1);
+
     return false;
 }
 
 /**
  * Sorts array of n values.
  */
-void sort(int values[], int n)
-{
+void sort(int values[], int n) {
     // TODO: implement an O(n^2) sorting algorithm
     return;
 }
