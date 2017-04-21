5835f1973af551aebfa95e4ebbc69144267410e3
Anton Zdanov <Implaier@users.noreply.github.com>
2017-04-21 13:13:05 +0300

    Update mario.c
    Another try.

--- PSET1/mario.c
+++ PSET1/mario.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
-    int max_height = 23;
+int main(void)
+{
     int height;
+    
     do {
-        printf("Height: ");
+        printf("Height: \n");
         height = get_int();
+    }
+    while (height < 0 || height > 23);
 
-        if (height == 0) {
-            return 0;
-        }
+    int padding = height - 1;
+    int row = 2;
+    
+    for (int i = height; i > 0; i--, padding--, row++) {
 
-    } while (height <= 0 || height > max_height);
-
-    int base = height + 1;
-    int blocks = 2;
-
-    for (int i = 0; i < height; i++, blocks++) {
-        for (int j = 0; j < base - blocks; j++) {
+        for (int j = padding; j > 0; j--) {
             printf(" ");
         }
-        for (int y = 0; y < blocks; y++) {
+
+        for (int k = row; k > 0; k--) {
             printf("#");
         }
+        
         printf("\n");
     }
+
+    return 0;
 }
