f938c001a9f12b9f4f53892c16d219556279c00d
Anton Zdanov <Implaier@users.noreply.github.com>
2017-04-27 22:31:08 +0300

    Update vigenere.c
    Naming

--- PSET2/vigenere.c
+++ PSET2/vigenere.c
@@ -6,11 +6,11 @@
 
 #define ALPHA_LEN 26
 
-bool is_string(string key);
+bool is_alpha_string(string key);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2 || is_string(argv[1]) == false) {
+    if (argc != 2 || is_alpha_string(argv[1]) == false) {
         printf("Usage: ./caesar k\n");
         return 1;
     }
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-bool is_string(string key)
+bool is_alpha_string(string key)
 {
     for (int i = 0, key_len = (int) strlen(key); i < key_len; ++i) {
         if (isalpha(key[i]) == false) {
