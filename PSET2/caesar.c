5f02ffa7d352dd04493f245d8042109ceeaac995
Anton Zdanov <Implaier@users.noreply.github.com>
2017-04-27 16:42:41 +0300

    Update caesar.c
    Another try

--- PSET2/caesar.c
+++ PSET2/caesar.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 #include <ctype.h>
 
-// constant
-#define ALPHA_NUM 26
+#define ALPHA_LEN 26
 
-int main(int argc, char *argv[]) {
-
-    // check argument number and argument key to be greater than -1
-    if (argc != 2 || atoi(argv[1]) < 0) {
-        printf("Usage (k >= 0):\n./caesar k \n");
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: ./caesar k\n");
         return 1;
     }
 
-    int key = atoi(argv[1]) % ALPHA_NUM;
+    int key = atoi(argv[1]) % ALPHA_LEN;
 
-    // get plaintext from user
-    printf("plaintext: ");
+    printf("plaintext:  ");
     string plaintext = get_string();
 
+    printf("ciphertext: ");
+    for (int i = 0, shift; plaintext[i] != '\0'; ++i) {
 
-    // encode plaintext
-    size_t text_len = strlen(plaintext);
-    char ciphertext[text_len + 1]; // + 1 for '\0'
-
-    for (int i = 0; i < text_len; i++) {
         if (isalpha(plaintext[i])) {
+
+            shift = plaintext[i] + key;
+
             if (isupper(plaintext[i])) {
-                // Uppercase shift
-                ciphertext[i] = (char) (((plaintext[i] + key - 'A') % ALPHA_NUM) + 'A');
-            } else {
-                // Lowercase shift
-                ciphertext[i] = (char) (((plaintext[i] + key - 'a') % ALPHA_NUM) + 'a');
+                printf("%c", (shift - 'A') % ALPHA_LEN + 'A');
             }
-        } else {
-            // Skip non alpha
-            ciphertext[i] = plaintext[i];
+            else {
+                printf("%c", (shift - 'a') % ALPHA_LEN + 'a');
+            }
+        }
+        else {
+            printf("%c", plaintext[i]);
         }
     }
 
-    printf("ciphertext: %s\n", ciphertext);
+    printf("\n");
 
     return 0;
 }
