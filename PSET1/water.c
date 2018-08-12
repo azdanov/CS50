9c5cb9a66f20338bf3f5a038523a0275fc6d4506
Anton Zdanov <azdanov@protonmail.com>
2018-08-12 08:18:00 +0300

    refactor(water): add constants for clarity

--- PSET1/water.c
+++ PSET1/water.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+const int ouncesPerMinute = 192;
+const int bottleCapacityOunces = 16;
+
 int main(void) {
-    printf("Minutes: ");
+  printf("Minutes: ");
 
-    int minutes = get_int();
+  int minutes = get_int();
 
-    printf("Bottles: %i\n", minutes * 192 / 16);
+  printf("Bottles: %i\n", minutes * ouncesPerMinute / bottleCapacityOunces);
 
-    return 0;
+  return 0;
 }
